4cf47010057413af0ab305f0728fe76b57dde809
azalea <alissaperalta@outlook.com>
2025-08-14 17:35:20 +0800

    add product constant

--- src/constants/product-category.ts
+++
@@ -1,7 +0,0 @@
-export const PRODUCT_CATEGORY = {
-  ALL: "all",
-  PROWARE: "proware",
-  UNIFORM: "uniform",
-  STATIONERY: "stationery",
-  ACCESSORIES: "accessories",
-};
---
+++ src/constants/product.ts
@@ -0,0 +1,19 @@
+export const PRODUCT_CATEGORY = {
+  ALL: "all",
+  PROWARE: "proware",
+  UNIFORM: "uniform",
+  STATIONERY: "stationery",
+  ACCESSORY: "accessory",
+};
+
+export const PRODUCT_SIZE = {
+  "Extra Small": "XS",
+  Small: "S",
+  Medium: "M",
+  Large: "L",
+  "Extra Large": "XL",
+  "2 Extra Large": "2XL",
+  "3 Extra Large": "3XL",
+  "4 Extra Large": "4XL",
+  "5 Extra Large": "5XL",
+};
