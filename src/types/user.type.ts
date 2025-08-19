376a8d9678d89602ac2f72e08721764a66787c5f
azalea <alissaperalta@outlook.com>
2025-08-19 19:04:58 +0800

    add student type

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,9 +1,24 @@
 import { IRoleSystemTag } from "./role.type";
 
 export interface IUser {
+  fullName: string;
   firstName: string;
   lastName: string;
   email: string;
   roleSystemTag: IRoleSystemTag;
   id: string;
+  student: IStudent;
+  createdAt: Date;
+  deletedAt: Date | null;
+  updatedAt: Date | null;
+}
+
+export interface IStudent {
+  id: number;
+  userId: string;
+  programId: string;
+  level: string;
+  createdAt: Date;
+  deletedAt: Date | null;
+  updatedAt: Date | null;
 }
