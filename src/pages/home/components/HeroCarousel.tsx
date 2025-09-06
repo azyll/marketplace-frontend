8db12e414f06d86864cd830a088f12f173a9cac8
azalea <alissaperalta@outlook.com>
2025-09-06 22:41:53 +0800

    update homepage

--- src/pages/home/components/HeroCarousel.tsx
+++ src/pages/home/components/HeroCarousel.tsx
@@ -1,5 +1,5 @@
 import { Carousel } from "@mantine/carousel";
-import { Image } from "@mantine/core";
+import { Image, Title } from "@mantine/core";
 import { getImage } from "@/services/media.service";
 import "@/styles/carousel.css";
 
@@ -13,28 +13,39 @@ const images = [
 
 export default function HeroCarousel() {
   return (
-    <Carousel
-      controlsOffset={"md"}
-      withIndicators
-      classNames={{ indicator: "indicator" }}
-      slideSize={{ base: "100%", md: "80%" }}
-      slideGap="lg"
-      emblaOptions={{
-        loop: true,
-        align: "center",
-      }}
-    >
-      {" "}
-      {images.map((src, index) => (
-        <Carousel.Slide key={index}>
-          <Image
-            h={{ base: "250px", md: "550px" }}
-            src={src}
-            alt={`Slide ${index + 1}`}
-            className="md:!rounded-xl"
-          />
-        </Carousel.Slide>
-      ))}
-    </Carousel>
+    <>
+      <section className="max-w-[1200px] mx-auto">
+        <Title
+          px={{ base: 16, xl: 0 }}
+          order={2}
+        >
+          Announcements
+        </Title>
+      </section>
+      <Carousel
+        pt={10}
+        pb={16}
+        controlsOffset={"md"}
+        withIndicators
+        classNames={{ indicator: "indicator" }}
+        slideSize={{ base: "100%", md: "80%" }}
+        slideGap="lg"
+        emblaOptions={{
+          loop: true,
+          align: "center",
+        }}
+      >
+        {images.map((src, index) => (
+          <Carousel.Slide key={index}>
+            <Image
+              h={{ base: "250px", md: "550px" }}
+              src={src}
+              alt={`Slide ${index + 1}`}
+              className="md:!rounded-xl"
+            />
+          </Carousel.Slide>
+        ))}
+      </Carousel>
+    </>
   );
 }
