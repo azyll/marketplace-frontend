876a7ab789ae8622f5d217b69d2999d30fcd5556
azalea <alissaperalta@outlook.com>
2025-09-11 23:06:17 +0800

    add isLoading to auth type

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -1,12 +1,13 @@
-import { IUser } from "./user.type";
+import { IUser } from "./user.type"
 
 export interface IAuthContext {
-  user: IUser | null;
-  setUser: (user: IUser | null) => void;
-  logout: () => void;
-  fetchUser: () => void;
+  user: IUser | null
+  setUser: (user: IUser | null) => void
+  logout: () => void
+  fetchUser: () => void
+  isLoading: boolean
 }
 
 export interface ILoginResponse {
-  accessToken: string;
+  accessToken: string
 }
