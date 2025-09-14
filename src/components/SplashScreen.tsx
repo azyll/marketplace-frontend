241383fc02bc24e6e276fa4a605b55b680623722
azalea <alissaperalta@outlook.com>
2025-09-14 22:56:02 +0800

    change splash screen logo

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,8 +1,10 @@
+import { Text, Title } from "@mantine/core"
+
 // components/SplashScreen.tsx
 export default function SplashScreen() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center bg-white">
-      <div className="flex h-15 w-15 items-center justify-center rounded-full bg-[#015582] shadow-md">
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-[#F2F5F9]">
+      <div className="flex h-20 w-20 items-center justify-center">
         <img
           src="/logo.png"
           alt="STIMERCH MARKET Logo"
