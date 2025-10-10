79daedb510219c4328cdb9f2632fd442b659af33
azalea <alissaperalta@outlook.com>
2025-10-10 22:18:53 +0800

    add sales endpoint, key, route, & type

---
+++ src/types/sale.type.ts
@@ -0,0 +1,31 @@
+import { IPagination } from "./common.type"
+import { IOrder } from "./order.type"
+
+export interface ISalesFilter extends IPagination {
+  from: string
+  to: string
+}
+
+export interface ISales {
+  id: string
+  total: number
+  oracleInvoice: string
+  orderId: string
+  order: IOrder
+  createdAt: Date
+  deletedAt: Date | null
+  updatedAt: Date | null
+}
+
+export interface IPaginatedResponseMeta {
+  currentPage: number
+  itemsPerPage: number
+  totalItems: number
+  totalSales: number
+}
+
+export interface IPaginatedSalesResponse {
+  message: string
+  data: ISales[]
+  meta: IPaginatedResponseMeta
+}
